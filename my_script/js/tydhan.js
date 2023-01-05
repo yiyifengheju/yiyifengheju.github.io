df2862da3e2fb69e326cb1a4aed327a3168a22cc
mastermao <wego_mao@hotmail.com>
2023-01-06 00:44:15 +0800

    Site updated: 2023-01-06 00:44:12

--- my_script/js/tydhan.js
+++ my_script/js/tydhan.js
@@ -11,8 +11,8 @@ var player0 = new APlayer({
     audio:{
             name: '他一定很爱你',
             artist: '阿杜',
-            url: 'https://my-gallery-1306340269.cos.ap-beijing.myqcloud.com/%E6%AD%8C%E6%9B%B2/%E9%98%BF%E6%9D%9C-%E4%BB%96%E4%B8%80%E5%AE%9A%E5%BE%88%E7%88%B1%E4%BD%A0.flac',
-            cover: '/my_script/cover/109951163076301483.jpg',
-            lrc: '/my_script/lrc/tydhan.lrc'
+            url: 'https://cdn.jsdelivr.net/gh/yiyifengheju/Music@master/Music/20230104/tydhan.mp3',
+            cover: 'https://cdn.jsdelivr.net/gh/yiyifengheju/Music@master/Music/20230104/ad.jpg',
+            lrc: 'https://cdn.jsdelivr.net/gh/yiyifengheju/Music@master/Music/20230104/tydhan.lrc'
         }
 });
