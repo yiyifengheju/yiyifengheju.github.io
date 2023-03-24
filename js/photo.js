83fe31d1b707eab3401851305841c42ea7708fa1
mastermao <wego_mao@hotmail.com>
2023-03-25 01:06:17 +0800

    Site updated: 2023-03-25 01:06:16

--- js/photo.js
+++ js/photo.js
@@ -21,7 +21,7 @@ var photo = {
             li += '<div class="card" style="width: auto; break-inside: avoid; box-sizing: border-box;">' +
                 '<div class="ImageInCard" style=" height: auto ">' +
                 '<a data-fancybox="gallery" href="' + pic_url + '">' +
-                '<img src="' + pic_url + '" width="95%"/>' + '</a>' +
+                '<img src="' + pic_url + '" width="98%"/>' + '</a>' +
                 '</div>' +
                 // '<div class="TextInCard">' + pic_name + '</div>' + //图片下显示文件名作为说明的功能
                 '</div>';
