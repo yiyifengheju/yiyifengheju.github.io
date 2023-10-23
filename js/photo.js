98cc9820020be29df041e55f73f7510aa3dc6a6e
mastermao <wego_mao@hotmail.com>
2023-10-24 00:43:19 +0800

    Site updated: 2023-10-24 00:43:18

--- js/photo.js
+++ js/photo.js
@@ -1,5 +1,5 @@
 var photo = {
-    page: 0,
+    page: 1,
     //offset 用于设置照片数量的上限
     offset: 1000,
     init: function () {
@@ -16,17 +16,13 @@ var photo = {
         if (begin >= data.length) return;
         var li = "";
         for (var i = begin; i < this.offset && i < data.length; i++) {
-            pic_url = data[i].toString().split(',')[0];
-            pic_name = data[i].toString().split(',')[1];
             li += '<div class="card" style="width: auto; break-inside: avoid; box-sizing: border-box;">' +
                 '<div class="ImageInCard" style=" height: auto ">' +
-                '<a data-fancybox="gallery" href="' + pic_url + '">' +
-                '<img src="' + pic_url + '" width="98%"/>' + '</a>' +
+                '<a data-fancybox="gallery" href="' + data[i] + '">' +
+                '<img src="' + data[i] + '"/>' + '</a>' +
                 '</div>' +
-                // '<div class="TextInCard">' + pic_name + '</div>' + //图片下显示文件名作为说明的功能
                 '</div>';
             console.log(data[i]);
-            li+='<div style="width: 20px"></div>';
         }
         $(".ImageGrid ").append(li);
         $(".ImageGrid ").lazyload();
